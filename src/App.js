874197b8fee2533e63ab4593061a963e07a22fa1
Alice Genkin <agenkin1@gmail.com>
2021-08-14 14:01:40 -0400

    api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class FetchAiportData extends React.Component {
+  state = {
+    loading: true
+  };
+
+  async componentDidMount() {
+    const url = "http://flare-code-exercise-data.s3.amazonaws.com/airlines.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  render() {
+    return (
+    <div>
+      {this.state.loading ? <div> Loading...</div> : <div>person...</div>  }
+      </div>
+    );
 }
+}
+// const Url = "http://flare-code-exercise-data.s3.amazonaws.com/airlines.json";
 
-export default App;
+// function App() {
+//   const [Data, setData] = useState({});
+
+//   useEffect(() => {
+//     getGitHubUserWithFetch();
+//   }, []);
+
+//   const getGitHubUserWithFetch = async () => {};
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h2>??</h2>
+//       </header>
+//       <div className="aiport-container">
+//         <h5 className="info-item">{Data.Airport.code}</h5>
+//         <h5 className="info-item">{Data.location}</h5>
+//         <h5 className="info-item">{Data.blog}</h5>
+//         <h5 className="info-item">{Data.company}</h5>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
